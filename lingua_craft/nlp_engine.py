a43d330c57d23a27a314a48fa1720ef5f26fd29c
Christophe Bouquet <christophe.productowner@gmail.com>
2024-04-07 16:42:15 +0200

    feature: Identifying and lemmatizing verbs

--- lingua_craft/nlp_engine.py
+++ lingua_craft/nlp_engine.py
@@ -5,5 +5,9 @@ nlp = spacy.load("it_core_news_lg")
 def identify_nouns(text):
     doc = nlp(text)
     nouns = [token.text for token in doc if token.pos_ == "NOUN"]
-    print(nouns)
     return nouns
+
+def identify_and_lemmatize_verbs(text):
+    doc = nlp(text)
+    verbs = [token.lemma_ for token in doc if token.pos_ == "VERB"]
+    return verbs
--- tests/test_nlp_engine.py
+++ tests/test_nlp_engine.py
@@ -1,6 +1,16 @@
-from lingua_craft.nlp_engine import identify_nouns
+from lingua_craft.nlp_engine import identify_nouns, identify_and_lemmatize_verbs
 
 def test_identify_nouns():
     text = "Il rapido volpe marrone salta sopra il cane pigro."
     expected_nouns = ["volpe", "cane"]
     assert identify_nouns(text) == expected_nouns
+    
+    
+def test_identify_and_lemmatize_verbs():
+    text = "La volpe salta alto."
+    expected_verbs = ["saltare"]
+    assert identify_and_lemmatize_verbs(text) == expected_verbs
+    
+    text = "I gatti dormivano sul divan"
+    expected_verbs = ["dormire"]
+    assert identify_and_lemmatize_verbs(text) == expected_verbs
