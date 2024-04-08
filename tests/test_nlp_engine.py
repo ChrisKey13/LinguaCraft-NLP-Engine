4164defe0cca48fecf8a7a01b391fca0b3fe9c44
Christophe Bouquet <christophe.productowner@gmail.com>
2024-04-08 12:15:41 +0200

    fix: ensure correct ordering of processed verbs

--- tests/test_nlp_engine.py
+++ tests/test_nlp_engine.py
@@ -1,16 +1,58 @@
-from lingua_craft.nlp_engine import identify_nouns, identify_and_lemmatize_verbs
+import pytest
+from lingua_craft.nlp_engine import identify_nouns, extract_pos_elements
 
 def test_identify_nouns():
     text = "Il rapido volpe marrone salta sopra il cane pigro."
     expected_nouns = ["volpe", "cane"]
     assert identify_nouns(text) == expected_nouns
     
-    
-def test_identify_and_lemmatize_verbs():
-    text = "La volpe salta alto."
-    expected_verbs = ["saltare"]
-    assert identify_and_lemmatize_verbs(text) == expected_verbs
-    
-    text = "I gatti dormivano sul divan"
-    expected_verbs = ["dormire"]
-    assert identify_and_lemmatize_verbs(text) == expected_verbs
+@pytest.mark.parametrize("text, expected_output", [
+    ("La volpe salta alto.",
+        {
+            "verbs": [
+                "saltare"
+            ],
+            "nouns": [
+                "volpe"
+            ],
+            "adjectives": [
+                "alto"
+            ],
+            "prepositions": []
+        }
+     ),
+    ("I gatti dormivano sul divano.",
+        {
+            "verbs": [
+                "dormire"
+            ],
+            "nouns": [
+                "divano", "gatti"
+            ],
+            "adjectives": [],
+            "prepositions": ["sul"]
+        }
+     ),
+    (
+        """
+        Giovanni Drogo, protagonista del romanzo, dopo l’accademia militare e con il grado di tenente viene assegnato alla Fortezza Bastiani: una caserma fortificata nel mezzo del deserto dei Tartari.
+        """, 
+        {
+            "verbs": [
+                "assegnare", "fortificare", "venire"
+            ],
+            "nouns": [
+                'accademia', 'caserma', 'deserto', 'grado', 'mezzo', 'protagonista', 'romanzo', 'tenente'
+            ],
+            "adjectives": [
+                "militare"
+            ],
+            "prepositions": [
+                'alla', 'con', 'dei', 'del', 'di', 'dopo', 'nel'
+            ]
+        }
+    )
+])
+
+def test_extract_pos_elements(text, expected_output):
+    assert extract_pos_elements(text) == expected_output
